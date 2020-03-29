65a7ad378e3d9e4bfa106d72b59ca8a3f1f92629
josephkiarie1 <62857999+josephkiarie1@users.noreply.github.com>
2020-03-29 16:58:34 -0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,112 @@
+export const storeProducts = [
+    {
+      id: 1,
+      title: "Van gogh",
+      img: "img/painting.jpg",
+      price: 10,
+      from: "france",
+      info:
+       "The most comprehensive primary source on Van Gogh is the correspondence between him and his younger brother, Theo.Their lifelong friendship, and most of what is known of Vincent's thoughts and theories of art, are recorded in the hundreds of letters they exchanged from 1872 until 1890.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "D & G perfume",
+      img: "img/perfume.jpg",
+      price: 16,
+      from: "france",
+      info:
+        "Dolce&Gabbana's first fragrance, named simply Dolce&Gabbana for Women, was launched in 1992. A men's version followed in 1994. The brand launched a series of fragrances over the year, with their Light Blue fragrances for men and women being among their most successful.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+
+    {
+      id: 3,
+      title: "powder & brush",
+      img: "img/powder & brush.jpg",
+      price: 18,
+      from: "Usa",
+      info:
+        " Morphe was born in 2008 among the artists and influencers in Los Angeles. Finally. A beauty brand created for the creators. For the dreamers. For those looking for killer makeup without killing their wallet. A place where our #MorpheBabe can let their passion and creativity for beauty fly high. We’re more than just a brand; we’re a ride-or-die fam.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    
+    {
+      id:4,
+      title:"Sofa",
+      img: "img/sofa.jpg",
+      price: 10,
+      from: "Iowa",
+      info:"A sofa bed or sofa-bed (in the US often called a sofabed, hide-a-bed, bed-couch, sleeper-sofa, or pullout sofa) is typically a sofa or couch that, underneath its seating cushions, hides a metal frame and thin mattress that can be unfolded or opened up to make a bed.",
+      inCart: false,
+      count: 0,
+      total:0
+    },
+    {
+      id: 5,
+      title: "Pineapples",
+      img: "img/pineapples.jpg",
+      price: 24,
+      from: "South America",
+      info:
+        "The pineapple (Ananas comosus) is a tropical plant with an edible fruit, also called a pineapple, and the most economically significant plant in the family Bromeliaceae. The pineapple is indigenous to South America, where it has been cultivated for many centuries.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      title: "Tomatoes",
+      img: "img/tomatoes.jpg",
+      price: 17,
+      from: "Mexico",
+      info:
+        "The tomato is the edible, often red, berry of the plant Solanum lycopersicum, commonly known as a tomato plant. The species originated in western South America and Central America. The Nahuatl (Aztec language) word tomatl gave rise to the Spanish word tomate, from which the English word tomato derived",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      title: "Oranges",
+      img: "img/oranges.jpg",
+      price: 30,
+      from: "China",
+      info:
+        "The orange is the fruit of the citrus species Citrus × sinensis in the family Rutaceae, native to China. It is also called sweet orange, to distinguish it from the related Citrus × aurantium, referred to as bitter orange.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 8,
+      title: "Strawberries",
+      img: "img/strawberries.jpg",
+      price: 2,
+      from: "France",
+      info:
+        "The garden strawberry (or simply strawberry ; Fragaria × ananassa) is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries, which are cultivated worldwide for their fruit. The fruit is widely appreciated for its characteristic aroma, bright red color, juicy texture, and sweetness.",
+      inCart: false,
+      count: 0,
+      total: 0
+    }
+  ];
+  
+  export const detailProduct = {
+    id: 1,
+    title: "Van gogh",
+    img: "img/painting.jpg",
+    price: 10,
+    from: "france",
+    info:
+    "An apple is a sweet, edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.",
+    inCart: false,
+    count: 0,
+    total: 0
+  };
